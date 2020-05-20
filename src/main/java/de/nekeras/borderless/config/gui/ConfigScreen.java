5f50b487af0f0d7cd7463211b35aa5d021ad31fb
Nico Müller <mueller@cryptospace.dev>
2020-05-20 23:35:15 +0200

    adjusted config screen and improved config screen labels

--- src/main/java/de/nekeras/borderless/config/gui/ConfigScreen.java
+++ src/main/java/de/nekeras/borderless/config/gui/ConfigScreen.java
@@ -1,9 +1,8 @@
 package de.nekeras.borderless.config.gui;
 
-import javax.annotation.Nonnull;
-
 import de.nekeras.borderless.config.FocusLossConfig;
 import de.nekeras.borderless.config.FullscreenModeConfig;
+import javax.annotation.Nonnull;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.widget.Widget;
 import net.minecraft.client.gui.widget.button.Button;
@@ -16,7 +15,13 @@ import net.minecraftforge.api.distmarker.OnlyIn;
 public class ConfigScreen extends Screen {
 
     private static final String TITLE_KEY = "borderless.config.title";
+    private static final String CHANGED_WARNING_KEY = "borderless.config.changed";
     private static final String DESCRIPTION_KEY_BASE = "borderless.%s";
+
+    private static final int LAYOUT_MAX_WIDTH = 250;
+    private static final int WHITE = 0xffffff;
+    private static final int YELLOW = 0xffff00;
+    private static final int LINE_HEIGHT = 25;
 
     private final Screen parent;
 
@@ -32,16 +37,18 @@ public class ConfigScreen extends Screen {
         assert minecraft != null;
         super.init();
 
+        int x = getHorizontalLayoutStart();
+
         addButton(
             ConfigScreenOption.FULLSCREEN_MODE.createWidget(minecraft.gameSettings,
-                (width - 250) / 2, (height - 100) / 2, 250));
+                x, LINE_HEIGHT * 2, LAYOUT_MAX_WIDTH));
 
         focusLossButton =
             addButton(ConfigScreenOption.FOCUS_LOSS.createWidget(minecraft.gameSettings,
-                (width - 250) / 2, (height - 50) / 2, 250));
+                x, LINE_HEIGHT * 3, LAYOUT_MAX_WIDTH));
 
-        addButton(new Button((width - 100) / 2, height - 50, 100, 20, I18n.format("gui.done"),
-            b -> onClose()));
+        addButton(new Button((width - 100) / 2, height - 75, 100, 20,
+            I18n.format("gui.done"), b -> onClose()));
     }
 
     @Override
@@ -55,15 +62,10 @@ public class ConfigScreen extends Screen {
 
     @Override
     public void render(int p_render_1_, int p_render_2_, float p_render_3_) {
-        assert minecraft != null;
         renderBackground();
-
-        drawCenteredString(minecraft.fontRenderer, title.getFormattedText(), width / 2, 20,
-            0xffffff);
-
-        minecraft.fontRenderer
-            .drawSplitString(I18n.format(getDescriptionKey()), (width - 250) / 2, height / 2,
-                250, 0xffffff);
+        renderTitle();
+        renderDescription();
+        renderChangedWarning();
 
         super.render(p_render_1_, p_render_2_, p_render_3_);
     }
@@ -74,6 +76,35 @@ public class ConfigScreen extends Screen {
         super.onClose();
 
         minecraft.displayGuiScreen(parent);
+    }
+
+    private void renderTitle() {
+        assert minecraft != null;
+
+        int x = width / 2;
+        int y = LINE_HEIGHT;
+
+        drawCenteredString(minecraft.fontRenderer, title.getFormattedText(), x, 20, WHITE);
+    }
+
+    private void renderDescription() {
+        assert minecraft != null;
+
+        int x = getHorizontalLayoutStart();
+        int y = LINE_HEIGHT * 4;
+
+        minecraft.fontRenderer
+            .drawSplitString(I18n.format(getDescriptionKey()), x, y, LAYOUT_MAX_WIDTH, WHITE);
+    }
+
+    private void renderChangedWarning() {
+        assert minecraft != null;
+
+        int x = getHorizontalLayoutStart();
+        int y = height - 50;
+
+        minecraft.fontRenderer
+            .drawSplitString(I18n.format(CHANGED_WARNING_KEY), x, y, LAYOUT_MAX_WIDTH, YELLOW);
     }
 
     private String getDescriptionKey() {
@@ -87,6 +118,14 @@ public class ConfigScreen extends Screen {
 
             return String.format("%s.%s", modeKey, focusLoss.name().toLowerCase());
         }
+    }
+
+    private int getHorizontalLayoutStart() {
+        return (width - LAYOUT_MAX_WIDTH) / 2;
+    }
+
+    private int getVerticalCenter() {
+        return height / 2;
     }
 
 }
